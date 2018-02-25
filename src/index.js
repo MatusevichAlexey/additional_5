ef87b0f1be30f97b13254f87dad75ebed5ecb482
MatusevichAlexey <alexeimatusevich97@gmail.com>
2018-02-25 17:12:23 +0300

    implement brackets chek function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var arr = str.split("");
+
+  var changes = true;
+  while (changes) {
+      changes = false;
+      for (var i = 0; i < arr.length-1; i++) {
+          for (var j = 0; j < bracketsConfig.length; j++) {
+              if ((arr[i] === bracketsConfig[j][0]) && (arr[i+1] === bracketsConfig[j][1])) {
+                  changes = true;
+                  arr.splice(i,2);
+                  i--;
+                  break;
+              } 
+          
+          }
+
+      }
+
+  }
+
+  if (arr.length === 0) {
+      return true;
+  } else {
+      return false;
+  }
 }
